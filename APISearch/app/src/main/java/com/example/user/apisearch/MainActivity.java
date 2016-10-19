ebe94a55e5cbd06c2b9fe56d4debd4fc3b6778b1
kjh44004 <kjh44004@naver.com>
2016-10-19 13:54:31 +0900

    2 비동기 실행

--- APISearch/app/src/main/java/com/example/user/apisearch/MainActivity.java
+++ APISearch/app/src/main/java/com/example/user/apisearch/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.user.apisearch;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -19,17 +22,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView view = (TextView) findViewById(R.id.textview);
+        AsyncTask asyncTask = new AsyncTask() {
+            @Override
+            protected Object doInBackground(Object[] params) {
+                getUrlContents("http://naver.com");
+                return null;
+            }
+        };
 
+        asyncTask.execute();
+    }
+
+    private static String getUrlContents(String theUrl) {
+        StringBuilder content = new StringBuilder();
+
+        // many of these calls can throw exceptions, so i've just
+        // wrapped them all in one try/catch statement.
         try {
-            URL url = new URL("https://openapi.naver.com/v1/search/blog.xml?query=%EB%A6%AC%EB%B7%B0&display=10&start=1&sort=sim");
+            // create a url object
+            URL url = new URL(theUrl);
+
+            // create a urlconnection object
             URLConnection urlConnection = url.openConnection();
-            urlConnection.getInputStream();
-            OutputStream outputStream = urlConnection.getOutputStream();
-            view.setText(outputStream.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+
+            // wrap the urlconnection in a bufferedreader
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String line;
+
+            // read from the urlconnection via the bufferedreader
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        return content.toString();
     }
 }
+
