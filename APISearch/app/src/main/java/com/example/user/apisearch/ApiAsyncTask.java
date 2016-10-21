7e9528f68f54c1e7ed7c57cff8c4108c6a0cd9a6
kjh44004 <kjh44004@naver.com>
2016-10-21 11:56:18 +0900

    먼말인지 하나도 모르겟다

--- APISearch/app/src/main/java/com/example/user/apisearch/ApiAsyncTask.java
+++ APISearch/app/src/main/java/com/example/user/apisearch/ApiAsyncTask.java
@@ -7,20 +7,20 @@ import android.speech.tts.Voice;
  * Created by User on 2016-10-21.
  */
 
-public class ApiAsyncTask<T> extends AsyncTask<Void, Void, T>  {
+public class ApiAsyncTask<T> extends AsyncTask <Void, Void, T> {
     private final String url;
-    private final Class<T> classOfT;
+    private final Class<T> classofT;
     private final ApiTaskCallback<T> runnable;
 
-    public ApiAsyncTask(String url, Class<T> classOfT, ApiTaskCallback runnable) {
-        this.url = url;
-        this.classOfT = classOfT;
+    public ApiAsyncTask(String url, Class<T> classofT, ApiTaskCallback<T> runnable) {   /*주소가 있어야지*/
+        this.url = url;                 /*this = 객체 자기 자신*/
+        this.classofT = classofT;
         this.runnable = runnable;
     }
 
     @Override
     protected T doInBackground(Void... params) {
-        return new ApiConnector().get(url, classOfT);
+        return new ApiConnector().get(url, classofT);
     }
 
     @Override
--- APISearch/app/src/main/java/com/example/user/apisearch/MainActivity.java
+++ APISearch/app/src/main/java/com/example/user/apisearch/MainActivity.java
@@ -1,11 +1,9 @@
 package com.example.user.apisearch;
 
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import java.util.List;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textview);
-        new ApiAsyncTask<ListModel>("http://14.49.231.5/a.json", ListModel.class, new ApiTaskCallback() {
+        new ApiAsyncTask<ListModel>("http://14.49.231.5/b.json", ListModel.class, new ApiTaskCallback<ListModel>() {
             @Override
-            public void run(Object result) {
-
+            public void run(ListModel result) {
+                Toast.makeText(MainActivity.this, result.list[0], Toast.LENGTH_LONG).show();
             }
         }).execute();
     }
